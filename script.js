faa372786cdf70a010773df092f4597aa1d30d22
Olie Auger <olie.auger@gmail.com>
2022-01-23 14:15:22 -0500

    Normal colorization

--- script.js
+++ script.js
@@ -84,7 +84,9 @@ void main() {
     vec3 light = normalize(u_light);
     vec3 normal = normalize(v_normal.xyz);
     float diffuse = max(dot(light, normal), 0.0) + 0.1;
-    gl_FragColor = vec4(diffuse, diffuse, diffuse, 1.0);
+    vec4 color = vec4(normal, 0.0) * 0.5 + 0.5;
+    gl_FragColor = vec4(color.xyz * diffuse, 1.0);
+    // gl_FragColor = vec4(diffuse, diffuse, diffuse, 1.0);
 }
 `;
 
@@ -104,53 +106,159 @@ gl.enable(gl.DEPTH_TEST);
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+function horizontalVerticalDistanceToXYZ(h, v, d) {
+    let x, y, z;
+    x = d * -Math.sin(h);
+    z = d * -Math.cos(h);
+    y = d * Math.sin(v);
+    x *= Math.cos(v);
+    z *= Math.cos(v);
+    return [x, y, z];
+}
+
 let currentMesh = null;
-let view = [0, 3, -10];
+let view = [0, 0, 2];
+let pitch = 0;
+let yaw = 0;
 
 function draw() {
     const projectionMatrix = mat4.create();
     const viewMatrix = mat4.create();
 
-    mat4.perspective(projectionMatrix, Math.PI / 4, canvas.width / canvas.height, 0.1, 100);
-    mat4.lookAt(viewMatrix, view, [0, 0, 0], [0, 1, 0]);
+    mat4.perspective(projectionMatrix, Math.PI / 4, canvas.width / canvas.height, 0.1, null);
+    mat4.translate(viewMatrix, viewMatrix, view);
+    mat4.rotateY(viewMatrix, viewMatrix, yaw);
+    mat4.rotateX(viewMatrix, viewMatrix, pitch);
+    mat4.invert(viewMatrix, viewMatrix);
+    // mat4.lookAt(viewMatrix, view, [0, 0, 0], [0, 1, 0]);
 
     const resultMatrix = mat4.create();
     mat4.multiply(resultMatrix, projectionMatrix, viewMatrix);
     gl.uniformMatrix4fv(matrixUniformLocation, false, resultMatrix);
+    gl.uniform3f(lightUniformLocation, view[0], view[1], view[2]);
     gl.clearColor(50 / 255, 168 / 255, 227 / 255, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     currentMesh.draw(gl, positionAttributeLocation, normalAttributeLocation);
 }
-
-addEventListener('keydown', e => {
-    let key = e.key.toLowerCase();
+let w = false;
+let s = false;
+let a = false;
+let d = false;
+let q = false;
+let e = false;
+addEventListener('keydown', event => {
+    let key = event.key.toLowerCase();
     if (key == 'w') {
-        view[2] += 1;
+        w = true;
     } else if (key == 's') {
-        view[2] -= 1;
+        s = true;
     } else if (key == 'a') {
-        view[0] -= 1;
+        a = true;
     } else if (key == 'd') {
-        view[0] += 1;
+        d = true;
     } else if (key == 'q') {
-        view[1] -= 1;
+        q = true;
     } else if (key == 'e') {
-        view[1] += 1;
+        e = true;
+    }
+    console.log(`e ${e ? 'on' : 'off'}`);
+});
+addEventListener('keyup', event => {
+    let key = event.key.toLowerCase();
+    if (key == 'w') {
+        w = false;
+    } else if (key == 's') {
+        s = false;
+    } else if (key == 'a') {
+        a = false;
+    } else if (key == 'd') {
+        d = false;
+    } else if (key == 'q') {
+        q = false;
+    } else if (key == 'e') {
+        e = false;
+    }
+    console.log(`e ${e ? 'on' : 'off'}`);
+});
+let lastTime = Date.now();
+function drawLoop() {
+    let now = Date.now();
+    let delta = now - lastTime;
+    lastTime = now;
+    let distance = delta / 1000 * 1.5;
+    if (w) {
+        let [x, y, z] = horizontalVerticalDistanceToXYZ(yaw, pitch, distance);
+        view[0] += x;
+        view[1] += y;
+        view[2] += z;
+    }
+    if (s) {
+        let [x, y, z] = horizontalVerticalDistanceToXYZ(yaw + Math.PI, pitch, distance);
+        view[0] += x;
+        view[1] += y;
+        view[2] += z;
+    }
+    if (d) {
+        let [x, y, z] = horizontalVerticalDistanceToXYZ(yaw + Math.PI * 1.5, pitch, distance);
+        view[0] += x;
+        view[1] += y;
+        view[2] += z;
+    }
+    if (a) {
+        let [x, y, z] = horizontalVerticalDistanceToXYZ(yaw + Math.PI * 0.5, pitch, distance);
+        view[0] += x;
+        view[1] += y;
+        view[2] += z;
+    }
+    if (q) {
+        let [x, y, z] = horizontalVerticalDistanceToXYZ(yaw, pitch - Math.PI / 2, distance);
+        view[0] += x;
+        view[1] += y;
+        view[2] += z;
+    }
+    if (e) {
+        let [x, y, z] = horizontalVerticalDistanceToXYZ(yaw, pitch + Math.PI / 2, distance);
+        view[0] += x;
+        view[1] += y;
+        view[2] += z;
     }
     draw();
+    requestAnimationFrame(drawLoop);
+}
+
+canvas.addEventListener('click', e => {
+    canvas.requestPointerLock();
+});
+
+addEventListener('mousemove', e => {
+    if (document.pointerLockElement) {
+        yaw -= e.movementX / 100;
+        pitch -= e.movementY / 100;
+        if (pitch < -Math.PI / 2) {
+            pitch = -Math.PI / 2;
+        }
+        if (pitch > Math.PI / 2) {
+            pitch = Math.PI / 2;
+        }
+    }
 });
 function process(url) {
-    parseObj(url).then(mesh => {
-        console.log('mesh', mesh);
-        let ray = new Ray(new Vector(0.01, 5, 0), new Vector(0, -1, 0), mesh, 10);
-        // ray.trace();
-        console.log('ray', ray);
-        if (useWebGL) {
-            currentMesh = mesh;
-            view = [0, 3, -10];
-            draw();
-            console.log('done');
-        }
+    return new Promise(resolve => {
+        parseObj(url).then(mesh => {
+            console.log('mesh', mesh);
+            let ray = new Ray(new Vector(0.01, 5, 0), new Vector(0, -1, 0), mesh, 10);
+            // ray.trace();
+            console.log('ray', ray);
+            if (useWebGL) {
+                currentMesh = mesh;
+                view = [0, 3, -10];
+                resolve();
+                console.log('done');
+            }
+        });
     });
 }
-process('./box.obj');
+process('./box.obj').then(() => {
+    lastTime = Date.now();
+    drawLoop();
+});
